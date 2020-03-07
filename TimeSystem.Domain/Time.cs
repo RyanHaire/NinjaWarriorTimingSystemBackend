3888bc51d0cc6921f70921f15832a65c79d24c2c
Ryan <RWHaire@hotmail.com>
2020-03-07 02:56:48 -0500

    * launchSettings.json: Updated user class and added time class
    * Time.cs:
    * User.cs:

---
+++ TimeSystem.Domain/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TimeSystem.Domain
+{
+    public class Time
+    {
+        public Time()
+        {
+        }
+
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime Date { get; set; }
+        public User user { get; set; }
+    }
+}
--- TimeSystem.Domain/User.cs
+++ TimeSystem.Domain/User.cs
@@ -7,11 +7,18 @@ namespace TimeSystem.Domain
     class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool Admin { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool Hidden { get; set; }
+        public bool Verified { get; set; }
 
     }
 }
